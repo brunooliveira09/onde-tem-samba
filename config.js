3810b4f1677ee5ce064ffc48c49c968acc26e4bc
Bruno Oliveira <brunooliveira@email.com>
2026-06-12 22:50:54 -0300

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@
 
 const OTS_CONFIG = {
   supabase: {
-    url: 'https://SEU_PROJECT_ID.supabase.co',
-    anonKey: 'SUA_ANON_KEY_AQUI',
+    url: 'https://cwdjdvsdswpisqidcayn.supabase.co/rest/v1/',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3ZGpkdnNkc3dwaXNxaWRjYXluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzMDI0ODEsImV4cCI6MjA5Njg3ODQ4MX0.yl_yleFrsNgQhOrN15op8EfK4LtfQ3UNHNH1-uh5svU',
   },
   map: {
     defaultCenter: [-23.2165, -47.5238], // Porto Feliz
